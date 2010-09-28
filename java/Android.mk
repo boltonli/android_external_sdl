924b730204064cc9dbf782cde75934c11ad5d088
Petr Havlena <havlenapetr@gmail.com>
2010-09-28 12:42:54 +0200

    add platform lib xml

--- java/Android.mk
+++ java/Android.mk
@@ -9,3 +9,20 @@ LOCAL_MODULE:= cz.havlena.sdl
 #LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_JAVA_LIBRARY)
+
+#--------------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cz.havlena.sdl.xml
+
+LOCAL_MODULE_TAGS := user
+
+LOCAL_MODULE_CLASS := ETC
+
+# This will install the file in /system/etc/permissions
+#
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
