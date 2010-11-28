6f9d8c8b6c3623a8b300a61618ee7a390412a43b
Petr Havlena <havlenapetr@gmail.com>
2010-11-28 10:18:56 +0100

    upgrade install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 adb shell mount -o remount,rw /dev/mtd/mtdblock4 /system
-adb push out/target/product/sapphire/system/framework/cz.havlena.sdl.jar system/framework
-adb push out/target/product/sapphire/system/lib/libsdl_jni.so system/lib
+echo installing cz.havlena.sdl.jar
+adb push ../../out/target/product/sapphire/system/framework/cz.havlena.sdl.jar system/framework
+echo installing libsdl_jni.so
+adb push ../../out/target/product/sapphire/system/lib/libsdl_jni.so system/lib
+echo installing libsdl.so
+adb push ../../out/target/product/sapphire/system/lib/libsdl.so system/lib
