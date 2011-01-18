3faac4b32c3c09df9fb1471ec006bd5ede20294c
havlenapetr <havlenapetr@gmail.com>
2011-01-18 09:48:27 +0100

    add forgotten sources, which is glue into ndk for our project

---
+++ Application.mk
@@ -0,0 +1,15 @@
+APP_PROJECT_PATH 	:= $(call my-dir)
+IN_NDK 			:= true
+TEST                    := true
+STATIC_BUILD            := true
+APP_PLATFORM            := android-4
+APP_BUILD_SCRIPT        := $(APP_PROJECT_PATH)/Android.mk
+APP_MODULES     	:= libnativehelper_static libtremor_static libsdlmixer_static libsdl_jni
+ifeq ($(STATIC_BUILD),true)
+APP_MODULES     	+= libsdl_static
+else
+APP_MODULES     	+= libsdl
+endif
+ifeq ($(TEST),true)
+APP_MODULES     	+= libsimplesdltest
+endif
